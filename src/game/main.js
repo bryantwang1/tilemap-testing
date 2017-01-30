9e8e460423cd10f6600227576e27b353caeb5367
Bryant <bryant.m.wang@gmail.com>
2017-01-30 14:46:35 -0800

    integrate pathfinding plugin with tilemap experiment.

--- src/game/main.js
+++ src/game/main.js
@@ -4,6 +4,7 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', { preload: p
 function preload() {
 
     game.load.image('ground_1x1', '../assets/tilemaps/tiles/ground_1x1.png');
+    game.load.image('car', '../assets/car90.png');
 
 }
 
@@ -11,6 +12,8 @@ var map;
 var layer1;
 var layer2;
 // var layer3;
+var pathfinder;
+var sprite;
 
 var marker;
 var currentTile = 0;
@@ -21,9 +24,10 @@ var showLayersKey;
 var layer1Key;
 var layer2Key;
 // var layer3Key;
+var blocked = false;
 
 function create() {
-
+    game.physics.startSystem(Phaser.Physics.ARCADE);
     game.stage.backgroundColor = '#2d2d2d';
 
     //  Creates a blank tilemap
@@ -70,8 +74,18 @@ function create() {
 
     console.log(layer1.index);
     console.log(layer2.index);
+    // console.log(map.layers); // map.createLayer automatically adds it to a property layers apparently.
     // console.log(layer3.index);
+    var walkables = [-1]; // -1 because we're using a layer with no other tiles, so any tile could be an obstacle
 
+    pathfinder = game.plugins.add(Phaser.Plugin.PathFinderPlugin);
+    pathfinder.setGrid(map.layers[1].data, walkables); // pass in layer2's data because it's the grid
+
+    sprite = game.add.sprite(50, 50, 'car');
+    sprite.anchor.setTo(0.5, 0.5);
+    game.physics.enable(sprite);
+
+    game.camera.follow(sprite);
 }
 
 function changeLayer(key) {
@@ -127,24 +141,49 @@ function updateMarker() {
 
 }
 
+function findPathTo(tilex, tiley) {
+
+    pathfinder.setCallbackFunction(function(path) {
+        path = path || [];
+        for(var i = 0, ilen = path.length; i < ilen; i++) {
+            map.putTile(4, path[i].x, path[i].y);
+        }
+        blocked = false;
+    });
+
+    pathfinder.preparePathCalculation([0,0], [tilex,tiley]);
+    pathfinder.calculatePath();
+}
+
 function update() {
 
-    if (cursors.left.isDown)
-    {
-        game.camera.x -= 4;
-    }
-    else if (cursors.right.isDown)
-    {
-        game.camera.x += 4;
-    }
+  // game.physics.arcade.collide(sprite, layer);
 
-    if (cursors.up.isDown)
+  sprite.body.velocity.x = 0;
+  sprite.body.velocity.y = 0;
+  sprite.body.angularVelocity = 0;
+
+  if (cursors.left.isDown)
+  {
+      sprite.body.angularVelocity = -200;
+  }
+  else if (cursors.right.isDown)
+  {
+      sprite.body.angularVelocity = 200;
+  }
+
+  if (cursors.up.isDown)
+  {
+      sprite.body.velocity.copyFrom(game.physics.arcade.velocityFromAngle(sprite.angle, 300));
+  }
+
+  marker.x = layer2.getTileX(game.input.activePointer.worldX) * 32;
+  marker.y = layer2.getTileY(game.input.activePointer.worldY) * 32;
+
+  if (game.input.mousePointer.isDown)
     {
-        game.camera.y -= 4;
-    }
-    else if (cursors.down.isDown)
-    {
-        game.camera.y += 4;
+        blocked = true;
+        findPathTo(layer2.getTileX(marker.x), layer2.getTileY(marker.y));
     }
 
 }
